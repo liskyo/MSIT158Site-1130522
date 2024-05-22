841c05af9fdfb9214e672107956b4c08bda811e2
sky <liskyo5470@gmail.com>
2024-05-22 16:54:17 +0800

    加入專案檔案。

---
+++ Controllers/AddressController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MSIT158Site.Models;
+using System.Security.Policy;
+
+namespace MSIT158Site.Controllers
+{
+    public class AddressController : Controller
+    {
+        private readonly MyDBContext _context;
+        public AddressController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            var cities = _context.Addresses.Select(a => a.City).Distinct().ToList();
+            return Json(cities);
+        }
+
+        [HttpGet]
+        public JsonResult GetSites(string city)
+        {
+            var sites = _context.Addresses.Where(a => a.City == city).Select(a => a.SiteId).Distinct().ToList();
+            return Json(sites);
+        }
+
+        [HttpGet]
+        public JsonResult GetRoads(string city, string siteId)
+        {
+            var roads = _context.Addresses.Where(a => a.City == city && a.SiteId == siteId).Select(a => a.Road).Distinct().ToList();
+            return Json(roads);
+        }
+    }
+}
